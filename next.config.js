4c4611efb27ed9b30fbd3ca2242bab2014426792
11xnt <allenterescencobq@gmail.com>
2023-03-17 18:10:40 +0000

    fix number idk

--- next.config.js
+++ next.config.js
@@ -5,7 +5,15 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
-  assetPrefix: isProd ? 'https://cdn.mydomain.com' : undefined,
+  assetPrefix: isProd ? 'https://irishcollegiateesports.azurewebsites.net/' : undefined,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: isProd ? 'https://irishcollegiateesports.azurewebsites.net/api/:path*' : undefined,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig

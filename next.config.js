124bbd73198dead3477d22bd4866dea55fa3691b
11xnt <allenterescencobq@gmail.com>
2023-03-17 18:27:47 +0000

    help me

--- next.config.js
+++ next.config.js
@@ -6,11 +6,32 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
   assetPrefix: isProd ? 'https://irishcollegiateesports.azurewebsites.net/' : undefined,
-  async rewrites() {
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       destination: isProd ? 'https://irishcollegiateesports.azurewebsites.net/:path*' : undefined,
+  //     },
+  //   ]
+  // },
+  async headers() {
     return [
       {
-        source: '/api/:path*',
-        destination: isProd ? 'https://irishcollegiateesports.azurewebsites.net/:path*' : undefined,
+        // matching all API routes
+        source: "/api/(.*)",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
       },
     ]
   },

78930ec9a7bc88f19aec353fb80bca81e3a1b15c
11xnt <allenterescencobq@gmail.com>
2023-03-17 19:38:00 +0000

    i want to die

--- next.config.js
+++ next.config.js
@@ -6,35 +6,35 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
   assetPrefix: isProd ? 'https://irishcollegiateesports.azurewebsites.net/' : undefined,
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/api/:path*',
-  //       destination: isProd ? 'https://irishcollegiateesports.azurewebsites.net/:path*' : undefined,
-  //     },
-  //   ]
-  // },
-  async headers() {
+  async rewrites() {
     return [
       {
-        // matching all API routes
-        source: "/api/(.*)",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://irishcollegiateesports.azurewebsites.net" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
+        source: '/api/:path*',
+        destination: isProd ? 'https://irishcollegiateesports.azurewebsites.net/:path*' : undefined,
       },
     ]
   },
+  // async headers() {
+  //   return [
+  //     {
+  //       // matching all API routes
+  //       source: "/api/(.*)",
+  //       headers: [
+  //         { key: "Access-Control-Allow-Credentials", value: "true" },
+  //         { key: "Access-Control-Allow-Origin", value: "https://irishcollegiateesports.azurewebsites.net" },
+  //         {
+  //           key: "Access-Control-Allow-Methods",
+  //           value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+  //         },
+  //         {
+  //           key: "Access-Control-Allow-Headers",
+  //           value:
+  //             "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+  //         },
+  //       ],
+  //     },
+  //   ]
+  // },
 }
 
 module.exports = nextConfig
